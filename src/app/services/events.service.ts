9dbaf0694c19b8b797a402c745832d7280ac67c8
Stoian Catalin <shaggy.cata@gmail.com>
2017-04-01 18:15:20 +0300

    remove cors problem

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -5,7 +5,6 @@ import {Http, Headers} from "@angular/http";
 @Injectable()
 export class EventsService {
 
-  events: Event[];
 
   constructor(private http: Http) { }
 
@@ -20,20 +19,8 @@ export class EventsService {
   }
 
   updateEvent(id, event) {
-    let header = new Headers({
-      'Content-Type': 'application/x-www-form-urlencoded'
-    });
-    let body = this.convertToBody(event);
-    return this.http.post(`http://localhost:3232/events/edit/${id}`, body, {headers: header})
+    return this.http.post(`http://localhost:3232/events/edit/${id}`, event)
       .map(response => response.json());
-  }
-
-  convertToBody(event) {
-    let body = '';
-    for (let property in event) {
-      body = body + property + '=' + event[property] + '&';
-    }
-    return body.substr(0, body.length - 1);
   }
 
 }
